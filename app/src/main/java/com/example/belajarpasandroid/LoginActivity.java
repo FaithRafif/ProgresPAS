2ce69055a8023fc66efb9e0c84b6b2d5b6b4cc33
armadaniDev <arma64560@gmail.com>
2026-05-30 21:44:37 +0700

    Feat : [AD] Redesign tampilan login

--- app/src/main/java/com/example/belajarpasandroid/LoginActivity.java
+++ app/src/main/java/com/example/belajarpasandroid/LoginActivity.java
@@ -4,25 +4,40 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText etUsername, etPassword;
+    CheckBox cbShowPass;
     Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin   = findViewById(R.id.btnLogin);
+        cbShowPass = findViewById(R.id.cbShowPass);
 
+        cbShowPass.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                etPassword.setTransformationMethod(
+                        HideReturnsTransformationMethod.getInstance());
+            } else {
+                etPassword.setTransformationMethod(
+                        PasswordTransformationMethod.getInstance());
+            }
+
+            etPassword.setSelection(etPassword.getText().length());
+        });
         btnLogin.setOnClickListener(v -> cekLogin());
     }
 
